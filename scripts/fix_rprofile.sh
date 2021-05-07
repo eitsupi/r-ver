68db0a6ef0336f6a965bc4cfbbc746a0e90c3608
eitsupi <ts1s1andn@gmail.com>
2021-05-07 14:52:02 +0900

    fix cran-fix script for to fix the env ver CRAN

--- scripts/fix_rprofile.sh
+++ scripts/fix_rprofile.sh
@@ -5,6 +5,7 @@ UBUNTU_VERSION=${UBUNTU_VERSION:-`lsb_release -sc`}
 CRAN=${CRAN:-https://cran.r-project.org}
 
 ## mechanism to force source installs if we're using RSPM
-CRAN_SOURCE=${CRAN/"__linux__/$UBUNTU_VERSION/"/""}
-
-echo 'if(R.version$arch == "aarch64") {options(repos = "'${CRAN_SOURCE}'")}' >> ${R_HOME}/etc/Rprofile.site
+if [ "$(uname -m)" = "aarch64" ]; then
+    export CRAN=${CRAN/"__linux__/$UBUNTU_VERSION/"/""}
+    echo 'options(repos = "'${CRAN}'")' >> ${R_HOME}/etc/Rprofile.site
+fi
